0b0d4640c10c7e6542c363b17344bdf3d17f97fe
Alla Oleksyn <allaoleksyn@gmail.com>
2023-04-17 11:22:44 +0300

    Delete unused comments

--- src/helpers/localStorage.ts
+++ src/helpers/localStorage.ts
@@ -1,12 +1,3 @@
 export const localStorageRemoveItem = (arr: string[]) => {
 	arr.forEach((el) => localStorage.removeItem(el));
 };
-// export const localStorageAddItem = <T>(obj: T) => {
-// 	Object.entries(obj).forEach(([key, value]) => {
-// 		if (typeof value !== 'string') {
-// 			localStorage.setItem(key, JSON.stringify(value));
-// 		} else {
-// 			localStorage.setItem(key, value);
-// 		}
-// 	});
-// };
